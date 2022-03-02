f58dec83a6989db0d832cf3272b795f959096cfb
Lenovo <Lenovo@Avinash>
2022-03-02 17:49:04 +0530

    Powered with Spark

--- sparkday37/src/main/scala/sparkday37/sparkobj.scala
+++ sparkday37/src/main/scala/sparkday37/sparkobj.scala
@@ -1,5 +1,20 @@
 package sparkday37
 
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import sys.process._
+
 object sparkobj {
   
+  	def main(args:Array[String]):Unit={
+
+			val conf = new SparkConf().setAppName("ES").setMaster("local[*]")
+					val sc = new SparkContext(conf)
+					sc.setLogLevel("ERROR")
+
+
+
+
+	}
 }
+  
