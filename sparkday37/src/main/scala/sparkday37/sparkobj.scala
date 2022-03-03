19fcebe0d9ffa952cf4cbf40277a97a57d9c2e17
Lenovo <Lenovo@Avinash>
2022-03-04 00:50:25 +0530

    CODE ADDED

--- sparkday37/src/main/scala/sparkday37/sparkobj.scala
+++ sparkday37/src/main/scala/sparkday37/sparkobj.scala
@@ -1,7 +1,9 @@
 package sparkday37
 
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 import sys.process._
 
 object sparkobj {
@@ -11,6 +13,14 @@ object sparkobj {
 			val conf = new SparkConf().setAppName("ES").setMaster("local[*]")
 					val sc = new SparkContext(conf)
 					sc.setLogLevel("ERROR")
+					
+					val spark =SparkSession.builder().getOrCreate()
+					import spark.implicits._
+
+					val df = spark.read.format("csv").option("header","true").load("file:///E:/Project 2021-22/data/txns")
+					df.show(5,false)
+
+					val df2 = df.filter(!col("category").isin("Gymnastics","Team Sports"))
 
 
 
